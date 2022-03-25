815f1993105ad8390ae3b5cc81e5bc469ea9286c
AkidaSoto <60154963+AkidaSoto@users.noreply.github.com>
2022-03-25 19:00:04 -0400

    2022 Update

--- Tests/backgroundlisten.py
+++ Tests/backgroundlisten.py
@@ -2,10 +2,6 @@ import time
 import pyttsx3
 import speech_recognition as sr
 import numpy as np
-import scipy.signal
-import matplotlib.pyplot as plt
-from pyaudio import PyAudio, paContinue, paFloat32, paInt16
-from time import sleep
 
 def say(s,typ):
    engine = pyttsx3.init()
@@ -18,75 +14,45 @@ def say(s,typ):
         engine.setProperty('rate', 200)
    
    voices = engine.getProperty('voices')
-   engine.setProperty('voice', voices[2].id)                                                                                          
+   engine.setProperty('voice', voices[0].id)                                                                                          
    engine.say(s)
    a = engine.runAndWait()
 
-fs            = 44100   # Hz
-threshold     = 0.8     # absolute gain
-delay         = 40      # samples
-release_coeff = 0.5555  # release time factor
-attack_coeff  = 0.5     # attack time factor
-block_length  = 1024    # samples
-
-
+charlie = False
 
 # this is called from the background thread
 def callback(recognizer, audio):
     # received audio data, now we'll recognize it using Google Speech Recognition
+    global charlie
     try:
-        # for testing purposes, we're just using the default API key
-        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-        # instead of `r.recognize_google(audio)`
-        audi = recognizer.recognize_wit(audio,'ZWNV4DTOJNPYBAY4HKNVZOZ44Q2DRUQF')
-
-        if audi:
+        audi = recognizer.recognize_google(audio)
+        
+        if 'charlie' in audi.lower() and not charlie:
+            charlie = True
+            say('Yes, John?',2)
+        elif charlie is True: 
             audio_data = np.frombuffer(audio.frame_data, dtype=np.int16)
             print("max amp is " + str(np.nanmax(audio_data)))
             print("Google Speech Recognition thinks you said ")
-
-            #def callback(in_data, frame_count, time_info, flag):
-            #    #print(callback.counter)
-            #    played_frames = callback.counter
-            #    callback.counter += frame_count
-            #    return audio_data[played_frames:callback.counter], paContinue
-
-            #callback.counter = 0
-            #pa = PyAudio()
-
-            #stream = pa.open(format = paInt16,
-            #     channels = 1,
-            #     rate = audio.sample_rate,
-            #     output = True,
-            #     stream_callback = callback)
-
-            #while stream.is_active():
-            #    sleep(0.1)
-
-            #stream.close()
-            #pa.terminate()
-
-            if np.nanmax(audio_data) > 1000:
+            charlie = False
+            if np.nanmax(audio_data) > 4000:
+                say('You said',2)
                 say(audi,2)
             else:
                 say('speak up please',2)
-            
-            
-
+                    
     except sr.UnknownValueError:
         print("Google Speech Recognition could not understand audio")
     except sr.RequestError as e:
         print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
-
-
 r = sr.Recognizer()
 m = sr.Microphone(device_index=1)
 
-say('Hi John, Give me a moment to calibrate',2)
+say('Hi User, Give me a moment to calibrate',2)
 print("Calibration...")
 with m as source:
-    r.adjust_for_ambient_noise(source,5)  # we only need to calibrate once, before we start listening
+    r.adjust_for_ambient_noise(source)
 
 # start listening in the background (note that we don't have to do this inside a `with` statement)
 say('Okay, go ahead',2)
